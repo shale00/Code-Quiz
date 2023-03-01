5947d0c5abea2921f10c5743f98db1bbc0e4761d
sth <sth0067@gmail.com>
2023-03-01 14:35:37 -0600

    added html, css, and js files. added header section to html and steps to js.

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+// Step 1 WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+//Step 2 WHEN I answer a question
+// THEN I am presented with another question
+
+// Step 3 WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+//Step 4 WHEN all questions are answered or the timer reaches 0
+// THEN the game is ove
+
+// Step 5 WHEN the game is over
+// THEN I can save my initials and score
